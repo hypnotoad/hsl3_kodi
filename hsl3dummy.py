7fd766574688892951b16d60c579f57e7e933f16
Ralf Dragon <hypnotoad@lindra.de>
2026-03-14 12:04:53 +0100

    Add header

--- hsl3dummy.py
+++ hsl3dummy.py
@@ -1,3 +1,24 @@
+#
+# hsl3dummy - dummy hsl3 framework
+#   Written by Ralf Dragon <hypnotoad@lindra.de>
+#   Copyright (C) 2026 Ralf Dragon
+#
+# This program is freely distributable per the following license:
+#
+#  Permission to use, copy, modify, and distribute this software and its
+#  documentation for any purpose and without fee is hereby granted,
+#  provided that the above copyright notice appears in all copies and that
+#  both that copyright notice and this permission notice appear in
+#  supporting documentation.
+#
+#  I DISCLAIM ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING ALL
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO EVENT SHALL I
+#  BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY
+#  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+#  WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,
+#  ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
+#  SOFTWARE.
+
 import json
 
 class DebugSection:
